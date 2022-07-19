3b29cd2279abd765a56393ab2ac17ebaade21328
RicardoBordon <92645460+RicardoBordon@users.noreply.github.com>
2022-07-19 17:55:15 -0300

    headers

--- controllers/authController.js
+++ controllers/authController.js
@@ -74,6 +74,11 @@ export const refreshToken = (req, res) => {
 }
 
 export const logout = (req, res) => {
-    res.clearCookie("refreshToken");
+    res.clearCookie("refreshToken", {
+        httpOnly: true,
+        secure: true,
+        expires: new Date(Date.now() + expiresIn * 1000),
+        sameSite: "none",
+    });
     res.json({ok: true});     
 };
